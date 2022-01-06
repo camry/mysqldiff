d47d9d574b0f2cddea6aee8ecb6e933e4460dcd3
camry <camry.chen@foxmail.com>
2022-01-06 20:48:30 +0800

    UPDATE Version.

--- cmd/root.go
+++ cmd/root.go
@@ -154,7 +154,7 @@ var (
     rootCmd = &cobra.Command{
         Use:     "mysqldiff",
         Short:   "差异 SQL 工具。",
-        Version: "v3.0.3",
+        Version: "v3.0.4",
         Run: func(cmd *cobra.Command, args []string) {
             sourceMatched, err1 := regexp.MatchString(HostPattern, source)
             dbMatched, err3 := regexp.MatchString(DbPattern, db)
